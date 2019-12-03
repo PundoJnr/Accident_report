9ad5590c71ef5874ab4d203093d629964fba1a3b
PundoJnr <pundoarthur@gmail.com>
2019-12-03 05:51:02 +0300

    Adds radio onClick

--- app/src/main/java/com/dhure/accidentreport/report.java
+++ app/src/main/java/com/dhure/accidentreport/report.java
@@ -12,6 +12,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 
@@ -21,13 +23,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class report extends AppCompatActivity {
 
+    RadioButton hurty, hurtn, vehy, vehn, owny, ownn;
+    RadioGroup one, two, three;
+
+    private String hurt ="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
         setTitle("Accident information");
-        setContentView(R.layout.activity_report);
+
+        initVIews();
+
+        final String[] hurt = {hurty.getText().toString()};
+
+        one.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.hYes:
+                        hurt[0] = "hurt";
+                        break;
+                    case R.id.hNo:
+                        hurt[0] = "notHurt";
+                }
+            }
+        });
+
+
+        sharedPref();
+    }
+
+    private void sharedPref() {
+    }
+
+    private void initVIews() {
+        hurty = findViewById(R.id.hYes);
+        hurtn = findViewById(R.id.hNo);
+        vehy = findViewById(R.id.vYes);
+        vehn = findViewById(R.id.vNo);
+        owny = findViewById(R.id.oYes);
+        ownn = findViewById(R.id.oNo);
+        one = findViewById(R.id.radio_group);
+        two =findViewById(R.id.radio_group2);
+        three = findViewById(R.id.radio_group3);
     }
 
     public  void next(View view){
